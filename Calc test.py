2db21a79e632b6a1621cfe5374759747b45e263b
Jech Azlie S. Rivera <jesa.rivera.ui@phinmaed.com>
2025-08-23 23:57:19 +0800

    calc.py

--- Calc test.py
+++ Calc test.py
@@ -1,9 +1,17 @@
 while True: 
     operator = input("Enter an operator (+ - * /): ")
-
-    num1 = float(input("Enter your 1st number: "))
-    num2 = float(input("Enter your 2nd number: "))
-
+    while True:
+        try:
+            num1 = float(input("Enter your 1st number: "))
+            break
+        except ValueError:
+            print("That is not a valid number." )
+    while True:
+        try:
+            num2 = float(input("Enter your 2nd number: "))
+            break
+        except ValueError:
+            print("That is not a valid number." )
     if operator == "+":
         result = num1 + num2
         print(round(result, 3))
@@ -20,11 +28,10 @@ while True:
             result = num1 / num2
             print(round(result, 3))
     else:
-        print(f"{operator} in an invalid operator")
+        print(f"{operator} is an invalid operator")
 
     choice = input("Would you like to try it again? (yes/no): ").lower()
     if choice == "no":
         print("Thank you for using this calculator!♡")
     break
-
 
