4b3c3642013e5d98487902e37cd1fe7bc4b35e95
Jech Azlie S. Rivera <jesa.rivera.ui@phinmaed.com>
2025-08-27 08:36:46 +0800

    Update Grading.py

--- Grading.py
+++ Grading.py
@@ -1,77 +1,77 @@
 while True:
     while True:
         try:
-            n1=float(input("Enter Marks in  General Mathematics:"))
+            sub1=float(input("Enter Marks in  General Mathematics:"))
             break
         except ValueError:
             print("Please enter a valid number.")
     while True:
         try:
-            n2=float(input("Enter Marks in Filipino:"))
+            sub2=float(input("Enter Marks in Filipino:"))
             break
         except ValueError:
             print("Please enter a valid number.")
     while True:
         try:
-            n3=float(input("Enter Marks in  Earth and Life Science:"))
+            sub3=float(input("Enter Marks in  Earth and Life Science:"))
             break
         except ValueError:
             print("Please enter a valid number.")
     while True:
         try:
-            n4=float(input("Enter Marks in  Physical Education:"))
+            sub4=float(input("Enter Marks in  Physical Education:"))
             break
         except ValueError:
             print("Please enter a valid number.")
     while True:
         try:
-            n5=float(input("Enter Marks in  Practical Research:"))
+            sub5=float(input("Enter Marks in  Practical Research:"))
             break
         except ValueError:
             print("Please enter a valid number.")
     while True:
         try:
-            n6=float(input("Enter Marks in  Oral Communication:"))
+            sub6=float(input("Enter Marks in  Oral Communication:"))
             break
         except ValueError:
             print("Please enter a valid number.")
     while True:
         try:
-            n7=float(input("Enter Marks in  Contemporary Philippine Arts from the Regions:"))
+            sub7=float(input("Enter Marks in  Contemporary Philippine Arts from the Regions:"))
             break
         except ValueError:
             print("Please enter a valid number.")
     while True:
         try:
-            n8=float(input("Enter Marks in  Personal Development:"))
+            sub8=float(input("Enter Marks in  Personal Development:"))
             break
         except ValueError:
             print("Please enter a valid number.")
     while True:
         try:
-            n9=float(input("Enter Marks in  Philosophy:"))
+            sub9=float(input("Enter Marks in  Philosophy:"))
             break
         except ValueError:
             print("Please enter a valid number.")
     while True:
         try:
-            n10=float(input("Enter Marks in  Understanding Culture, Politics, and Society:"))
+            sub10=float(input("Enter Marks in  Understanding Culture, Politics, and Society:"))
             break
         except ValueError:
             print("Please enter a valid number.")
     while True:
         try:
-            n11=float(input("Enter Marks in  Applied Economics:"))
+            sub11=float(input("Enter Marks in  Applied Economics:"))
             break
         except ValueError:
             print("Please enter a valid number.")
     while True:
         try:
-            n12=float(input("Enter Marks in  Reading and Writing:"))
+            sub12=float(input("Enter Marks in  Reading and Writing:"))
             break
         except ValueError:
             print("Please enter a valid number.")
-    sum=n1+n2+n3+n4+n5+n6+n7+n8+n9+n10+n11+n12
+    sum=sub1+sub2+sub3+sub4+sub5+sub6+sub7+sub8+sub9+sub10+sub11+sub12
     print("The Sum is:", sum)
 
     percentage=(sum/1200)*100
@@ -105,5 +105,6 @@ while True:
     if choice == "no":
         print("Okay, goodbye! Good luck with your studies!")
         break
+
 
 
